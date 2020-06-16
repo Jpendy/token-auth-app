8691b2c41336438376c6bff058e6ddf71d8b1d0e
Jpendy <Jpendy256@gmail.com>
2020-06-16 16:34:13 -0700

    added REST routes and tests for dogs

--- __tests__/dogs.test.js
+++ __tests__/dogs.test.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongod = new MongoMemoryServer();
 const mongoose = require('mongoose');
@@ -6,6 +7,7 @@ const User = require('../lib/models/User');
 
 const request = require('supertest');
 const app = require('../lib/app');
+const Dog = require('../lib/models/Dog');
 
 describe('token-auth-app routes', () => {
   beforeAll(async() => {
@@ -23,17 +25,121 @@ describe('token-auth-app routes', () => {
   });
 
   let user;
-
+  let dog;
   beforeEach(async() => {
     user = await User.create({
       email: 'jake@jake.com',
       password: 'jakePassword',
       profileImage: 'placeholder image url'
     });
+
+    dog = await Dog.create({
+      user: user._id,
+      name: 'Leo',
+      breedName: 'Black Lab'
+    });
   });
 
   it('it creates a new dog but only if user is signed in and verified', () => {
-      
+    
+    const agent = request.agent(app);
+
+    return agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'jake@jake.com',
+        password: 'jakePassword',
+      })
+      .then(() => {
+        return agent
+          .post('/api/v1/dogs')
+          .send({
+            user: user._id,
+            name: 'Leo',
+            breedName: 'Black Lab'
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              _id: expect.anything(),
+              user: user.id,
+              name: 'Leo',
+              breedName: 'Black Lab',
+              __v: 0
+            });
+          });
+      });
   });
 
+  it('it gets a dog by id with GET', () => {
+    const agent = request.agent(app);
+
+    return agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'jake@jake.com',
+        password: 'jakePassword',
+      })
+      .then(() => {
+        return agent
+          .get(`/api/v1/dogs/${dog._id}`)
+          .then(res => {
+            expect(res.body).toEqual({
+              _id: expect.anything(),
+              user: user.id,
+              name: 'Leo',
+              breedName: 'Black Lab',
+              __v: 0
+            });
+          });
+      });
+  });
+
+  it('it updates a dog if user is logged in', () => {
+    const agent = request.agent(app);
+
+    return agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'jake@jake.com',
+        password: 'jakePassword',
+      })
+      .then(() => {
+        return agent
+          .patch(`/api/v1/dogs/${dog._id}`)
+          .send({ name: 'Ozlo' })
+          .then(res => {
+            expect(res.body).toEqual({
+              _id: expect.anything(),
+              user: user.id,
+              name: 'Ozlo',
+              breedName: 'Black Lab',
+              __v: 0
+            });
+          });
+      });
+  });
+
+  it('it deletes a dog if user is logged in', () => {    
+    const agent = request.agent(app);
+
+    return agent
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'jake@jake.com',
+        password: 'jakePassword',
+      })
+      .then(() => {
+        return agent
+          .delete(`/api/v1/dogs/${dog._id}`)
+          .then(res => {
+            expect(res.body).toEqual({
+              _id: expect.anything(),
+              user: user.id,
+              name: 'Leo',
+              breedName: 'Black Lab',
+              __v: 0
+            });
+          });
+      });
+  });
 });
